66fcc488975cd1cec5e9781ecaa495538c48c22c
Kuba Ober <kuba@mareimbrium.org>
2014-09-15 23:09:18 -0400

    Port SubGLWindow to Qt.

--- src/gui_and_draw/SubGLWindow.h
+++ src/gui_and_draw/SubGLWindow.h
@@ -1,94 +1,45 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+//
+//////////////////////////////////////////////////////////////////////
+
 #ifndef _VSP_GUI_2D_GL_WINDOW_H
 #define _VSP_GUI_2D_GL_WINDOW_H
 
-#include <FL/Fl.H>
-#include <FL/Fl_Gl_Window.H>
-
 #include "DrawObj.h"
+#include <QGLWidget>
 
 namespace VSPGraphic
 {
 class GraphicEngine;
-class Renderable;
 }
-
-class FuselageGeom;
 
 namespace VSPGUI
 {
-/*
-* This class provides all functionalites of a 2D opengl window.
-*/
-class VspSubGlWindow : public Fl_Gl_Window
+
+class VspSubGlWindowPrivate;
+/// This class provides all functionalites of a 2D opengl window.
+class VspSubGlWindow : public QGLWidget
 {
+    Q_OBJECT
+    Q_DECLARE_PRIVATE( VspSubGlWindow )
+    QScopedPointer< VspSubGlWindowPrivate > const d_ptr;
 public:
-    /*
-    * Constructor.
-    * Parmeters are left upper corner position of the window (x, y) and
-    * window's width and height (w, h).
-    *
-    * drawObjScreen - specific drawObj screen to link.
-    */
-    VspSubGlWindow( int x, int y, int w, int h, DrawObj::ScreenEnum drawObjScreen );
-    /*
-    * Destructor.
-    */
-    virtual ~VspSubGlWindow();
+    /*! \param drawObjScreen specific drawObj screen to link. */
+    VspSubGlWindow( DrawObj::ScreenEnum drawObjScreen, QWidget * parent = 0 );
+    ~VspSubGlWindow();
 
-public:
-    /*
-    * Initialize window on first call.
-    */
-    virtual void show();
-    /*
-    * Draw 2D Scene.
-    */
-    virtual void draw();
-
-public:
-    /*
-    * Update window.
-    */
     virtual void update();
-
-    /*
-    * Set Zoom value.
-    */
     virtual void setZoomValue( float value );
 
-public:
-    /*!
-    * Get graphic engine of this window.
-    */
-    VSPGraphic::GraphicEngine * getGraphicEngine()
-    {
-        return m_GEngine;
-    }
-
+    VSPGraphic::GraphicEngine * getGraphicEngine();
 protected:
-    /*
-    * Initialize GLEW.
-    */
-    virtual void _initGLEW();
-    /*
-    * Update Scene.
-    */
-    virtual void _update( std::vector<DrawObj *> objects );
-
-private:
-    void _loadPointData( VSPGraphic::Renderable * destObj, DrawObj * drawObj );
-    void _loadLineData( VSPGraphic::Renderable * destObj, DrawObj * drawObj );
-
-protected:
-    /*
-    * VSP Graphic Engine.
-    */
-    VSPGraphic::GraphicEngine * m_GEngine;
-    unsigned int m_id;
-
-private:
-    DrawObj::ScreenEnum m_LinkedScreen;
-    bool m_Initialized;
+    void initializeGL() Q_DECL_OVERRIDE;
+    void paintGL() Q_DECL_OVERRIDE;
+    void resizeGL( int w, int h ) Q_DECL_OVERRIDE;
 };
+
 }
 #endif
