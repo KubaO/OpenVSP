52ff6407b8857b88e5b083ffff720acd8a4920e3
Kuba Ober <kuba@mareimbrium.org>
2014-10-09 02:01:47 -0400

    Port ParmScren to Qt.

--- src/gui_and_draw/ParmScreen.cpp
+++ src/gui_and_draw/ParmScreen.cpp
@@ -2,184 +2,163 @@
 // This file is released under the terms of the NASA Open Source Agreement (NOSA)
 // version 1.3 as detailed in the LICENSE file which accompanies this software.
 //
-
-// parmScreen.cpp: implementation of the geomScreen class.
 //
 //////////////////////////////////////////////////////////////////////
 
+/* A lot of this code was commented out. The Qt port approximates the inferred functionality
+ * of the original code. It has to be reviewed what are this screen's use cases, and the code
+ * has to be fixed accordingly.
+ */
 #include "ParmScreen.h"
 #include "ParmMgr.h"
+#include "LinkMgr.h"
 #include "StringUtil.h"
+#include "ui_ParmScreen.h"
+#include "VspScreenQt_p.h"
 
-#include <FL/Fl_File_Chooser.H>
+#include <QDialog>
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
+using std::string;
 
-ParmScreen::ParmScreen( ScreenMgr* mgr )  : VspScreenFLTK( mgr )
+class ParmScreen::Private : public QDialog, public VspScreenQt::Private
 {
+    Q_OBJECT
+    Q_PRIVATE_SLOT( self(), void SetUpdateFlag() )
+    VSP_DECLARE_PUBLIC( ParmScreen )
 
-    ParmUI* ui = m_ParmUI = new ParmUI();
-    m_FLTK_Window = ui->UIWindow;
+    Ui::ParmScreen ui;
+    string currParm;
 
-    ui->UIWindow->position( 780, 30 );
-    ui->linkToBrowser->callback( staticScreenCB, this );
-    ui->linkFromBrowser->callback( staticScreenCB, this );
-    ui->setParmAButton->callback( staticScreenCB, this );
-    ui->setParmBButton->callback( staticScreenCB, this );
-    ui->parmDescription->buffer( &m_TextBuffer );
-    ui->parmDescription->wrap_mode( Fl_Text_Display::WRAP_AT_BOUNDS, 0 );
+    Private( ParmScreen * q );
+    QWidget * widget() Q_DECL_OVERRIDE { return this; }
+    bool Update() Q_DECL_OVERRIDE;
+    void Update( Parm * );
+    void SelectToLink( int sel );
+    void SelectFromLink( int sel );
 
+    Q_SLOT void disabled_on_linkToBrowser_currentRowChanged( int row )
+    {
+        SelectToLink( row );
+    }
+    Q_SLOT void disabled_on_linkFromBrowser_currentRowChanged( int row )
+    {
+        SelectFromLink( row );
+    }
+    Q_SLOT void disabled_on_setParmAButton_clicked()
+    {
+        LinkMgr.SetCurrLinkIndex( -1 );
+        LinkMgr.SetParm( true, currParm );
+    }
+    Q_SLOT void disabled_on_setParmBButton_clicked()
+    {
+        LinkMgr.SetCurrLinkIndex( -1 );
+        LinkMgr.SetParm( false, currParm );
+    }
+};
+VSP_DEFINE_PRIVATE( ParmScreen )
+
+ParmScreen::Private::Private( ParmScreen * q ) :
+    VspScreenQt::Private( q )
+{
+    ui.setupUi( this );
+    ui.nameBox->setObjectName( "sectionHeader_0 ");
+    move( 780, 30 );
+    BlockSignalsInUpdates();
+    ConnectUpdateFlag();
 }
 
-//==== Update Screen ====//
-bool ParmScreen::Update()
+ParmScreen::ParmScreen( ScreenMgr* mgr ) :
+    VspScreenQt( *new ParmScreen::Private( this ), mgr )
 {
-    Parm* parm_ptr = ParmMgr.GetActiveParm();
+}
 
-    if ( !parm_ptr )
+bool ParmScreen::Private::Update()
+{
+    Parm * const active = ParmMgr.GetActiveParm();
+    if ( ! active )
     {
-        Hide();
+        q_func()->Hide();
         return false;
     }
 
-    m_NameString = parm_ptr->GetName();
-    m_ParmUI->nameBox->label( m_NameString.c_str() );
-
-    m_DescriptString = parm_ptr->GetDescript();
-    m_TextBuffer.text( m_DescriptString.c_str() );
-
-
-    //char str[256];
-    //sprintf( nameStr, "%s", parm_ptr->GetName().c_str() );
-    //m_ParmUI->nameBox->label( nameStr );
-
-
-
-
-    //parmUI->linkToBrowser->clear();
-    //parmUI->linkFromBrowser->clear();
-
-    //vector< ParmLink* > plVec = parmLinkMgrPtr->GetParmLinkVec();
-    //for ( int i = 0 ; i < (int)plVec.size() ; i++ )
-    //{
-    //  if ( plVec[i]->GetParmA() == p )
-    //      parmUI->linkToBrowser->add( plVec[i]->GetParmB()->get_name().get_char_star() );
-    //  if ( plVec[i]->GetParmB() == p )
-    //      parmUI->linkFromBrowser->add( plVec[i]->GetParmA()->get_name().get_char_star() );
-    //}
-
-
+    currParm = active->GetID();
+    Update( active );
     return true;
 }
 
-//==== Show Screen ====//
-void ParmScreen::Show()
+void ParmScreen::Private::Update( Parm * p )
 {
-    if ( Update() )
+    string pId = p->GetID();
+    ui.nameBox->setText( p->GetName().c_str() );
+    ui.parmDescription->setText( p->GetDescript().c_str() );
+
+    ui.linkedFromBrowser->clear();
+    ui.linkedToBrowser->clear();
+    const int N = LinkMgr.GetNumLinks();
+    for ( int i = 0; i < N; ++ i )
     {
-        m_FLTK_Window->show();
+        Link * const link = LinkMgr.GetLink( i );
+        if ( link->GetParmA() == pId )
+            ui.linkedToBrowser->addItem( link->GetParmB().c_str() );
+        if ( link->GetParmB() == pId )
+            ui.linkedFromBrowser->addItem( link->GetParmA().c_str() );
     }
 }
 
-
-//==== Callbacks ====//
-void ParmScreen::CallBack( Fl_Widget *w )
+void ParmScreen::Update( const std::string & parm )
 {
-
-
+    Parm * const p = ParmMgr.FindParm( parm );
+    if ( p ) d_func()->Update( p );
 }
 
+void ParmScreen::Show( const std::string & parm )
+{
+    Update( parm );
+    VspScreenQt::Show();
+}
 
+void ParmScreen::Private::SelectToLink( int sel )
+{
+    int const N = LinkMgr.GetNumLinks();
+    int count = 0;
+    for ( int i = 0; i < N; ++ i )
+    {
+        if ( LinkMgr.GetLink(i)->GetParmA() == currParm )
+        {
+            count++;
+            if ( count == sel )
+            {
+                LinkMgr.SetCurrLinkIndex( i );
+                /// \todo
+                /// Renew is used in place of old parmLinkMgrPtr->RebuildAllLink();
+                /// Is this correct? - KO
+                LinkMgr.Renew();
+            }
+        }
+    }
+}
 
-//void ParmScreen::update( Parm* p )
-//{
-//  currParm = p;
-//  sprintf( nameStr, "%s", p->get_name().get_char_star() );
-//  parmUI->nameBox->label( nameStr );
-//
-//  parmUI->linkToBrowser->clear();
-//  parmUI->linkFromBrowser->clear();
-//
-//  vector< ParmLink* > plVec = parmLinkMgrPtr->GetParmLinkVec();
-//  for ( int i = 0 ; i < (int)plVec.size() ; i++ )
-//  {
-//      if ( plVec[i]->GetParmA() == p )
-//          parmUI->linkToBrowser->add( plVec[i]->GetParmB()->get_name().get_char_star() );
-//      if ( plVec[i]->GetParmB() == p )
-//          parmUI->linkFromBrowser->add( plVec[i]->GetParmA()->get_name().get_char_star() );
-//  }
-//
-//
-//}
-//
-//
-//void ParmScreen::show(Parm* p)
-//{
-//  update(p);
-//  parmUI->UIWindow->show();
-//}
-//
-//void ParmScreen::screenCB( Fl_Widget* w )
-//{
-//  if (  w == parmUI->linkToBrowser )
-//  {
-//      int sel = parmUI->linkToBrowser->value();
-//      SelectToLink( sel );
-//  }
-//  else if (  w == parmUI->linkFromBrowser )
-//  {
-//      int sel = parmUI->linkFromBrowser->value();
-//      SelectFromLink( sel );
-//  }
-//  else if ( w == parmUI->setParmAButton )
-//  {
-//      parmLinkMgrPtr->SetCurrParmLinkIndex(-1);
-//      parmLinkMgrPtr->SetParmA( currParm );
-//  }
-//  else if ( w == parmUI->setParmBButton )
-//  {
-//      parmLinkMgrPtr->SetCurrParmLinkIndex(-1);
-//      parmLinkMgrPtr->SetParmB( currParm );
-//  }
-//
-//
-//}
-//
-//void ParmScreen::SelectToLink( int sel )
-//{
-//  vector< ParmLink* > plVec = parmLinkMgrPtr->GetParmLinkVec();
-//  int count = 0;
-//  for ( int i = 0 ; i < (int)plVec.size() ; i++ )
-//  {
-//      if ( plVec[i]->GetParmA() == currParm )
-//      {
-//          count++;
-//          if ( count == sel )
-//          {
-//              parmLinkMgrPtr->SetCurrParmLinkIndex( i );
-//              parmLinkMgrPtr->RebuildAllLink();
-//          }
-//      }
-//  }
-//}
-//
-//void ParmScreen::SelectFromLink( int sel )
-//{
-//  vector< ParmLink* > plVec = parmLinkMgrPtr->GetParmLinkVec();
-//  int count = 0;
-//  for ( int i = 0 ; i < (int)plVec.size() ; i++ )
-//  {
-//      if ( plVec[i]->GetParmB() == currParm )
-//      {
-//          count++;
-//          if ( count == sel )
-//          {
-//              parmLinkMgrPtr->SetCurrParmLinkIndex( i );
-//              parmLinkMgrPtr->RebuildAllLink();
-//          }
-//      }
-//  }
-//}
+void ParmScreen::Private::SelectFromLink( int sel )
+{
+    int const N = LinkMgr.GetNumLinks();
+    int count = 0;
+    for ( int i = 0; i < N; ++ i )
+    {
+        if ( LinkMgr.GetLink( i )->GetParmB() == currParm )
+        {
+            count++;
+            if ( count == sel )
+            {
+                LinkMgr.SetCurrLinkIndex( i );
+                /// \todo
+                /// Renew is used in place of old parmLinkMgrPtr->RebuildAllLink();
+                /// Is this correct? - KO
+                LinkMgr.Renew();
+            }
+        }
+    }
+}
 
+ParmScreen::~ParmScreen() {}
+
+#include "ParmScreen.moc"
--- src/gui_and_draw/ParmScreen.h
+++ src/gui_and_draw/ParmScreen.h
@@ -2,50 +2,27 @@
 // This file is released under the terms of the NASA Open Source Agreement (NOSA)
 // version 1.3 as detailed in the LICENSE file which accompanies this software.
 //
-
-// parmScreen.h: interface for the geomScreen class.
-// J.R. Gloudemans
+//
 //////////////////////////////////////////////////////////////////////
 
-#if !defined(AFX_PARMSCREEN_H__015D1220_857A_11D7_AC31_0003473A025A__INCLUDED_)
-#define AFX_PARMSCREEN_H__015D1220_857A_11D7_AC31_0003473A025A__INCLUDED_
+#ifndef PARMSCREEN_H__INCLUDED__
+#define PARMSCREEN_H__INCLUDED__
 
-#include "ScreenBase.h"
-#include "GuiDevice.h"
+#include "VspScreenQt.h"
 
-#include "parmFlScreen.h"
-#include "ScreenMgr.h"
+class Parm;
 
-using std::string;
-
-class ParmScreen : public VspScreenFLTK
+class ParmScreen : public VspScreenQt
 {
+    VSP_DECLARE_PRIVATE( ParmScreen )
 public:
     ParmScreen( ScreenMgr* mgr );
-    virtual ~ParmScreen()           {}
+    ~ParmScreen();
 
-    void Show();
-    bool Update();
-
-    void CallBack( Fl_Widget *w );
-    static void staticScreenCB( Fl_Widget *w, void* data )
-    {
-        ( ( ParmScreen* )data )->CallBack( w );
-    }
-
-    void SelectToLink( int sel );
-    void SelectFromLink( int sel );
-
-protected:
-
-    string m_ParmID;
-    ParmUI* m_ParmUI;
-
-    string m_NameString;
-    string m_DescriptString;
-
-    Fl_Text_Buffer m_TextBuffer;
-
+    /// \todo This method is not used anywhere.
+    void Update( const std::string & parm );
+    /// \todo This method is not used anywhere.
+    void Show( const std::string & parm );
 };
 
-#endif
+#endif // PARMSCREEN_H__INCLUDED__
